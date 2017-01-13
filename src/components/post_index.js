9a9f81db9a8bfc06dacd72753491e4b7d5ff4d34
Rickard <rickardkamel@gmail.com>
2017-01-13 13:15:11 +0100

    added postsList to index.

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,11 +1,24 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {fetchPosts} from '../actions/index';
-import {Link} from 'react-router';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
     componentWillMount() {
         this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        return this.props.posts.map((post) => {
+            return (
+                <li className='list-group-item' key={post.id}>
+                <Link to={"posts/" + post.id}>
+                    <span className='pull-xs-right'>{post.categories}</span>
+                    <strong>{post.title}</strong>
+                </Link>
+                </li>
+            );
+        });
     }
 
     render() {
@@ -16,15 +29,22 @@ class PostsIndex extends Component {
                         Add a post
                     </Link>
                 </div>
-                List of blog posts.
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
 
         );
     };
 }
 
+function mapStateToProps(state) {
+    return { posts: state.posts.all };
+}
+
 // function mapDispatchToProps(dispatch){
 //     return bindActionCreators({ fetchPosts }, dispatch);
 // }
 
-export default connect(null, {fetchPosts})(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
